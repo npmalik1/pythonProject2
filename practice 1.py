be75d564626728229fb938a5bfa14d42ac9238c0
npmalik1 <nirupamamalik171@gmail.com>
2023-05-12 18:12:48 +0530

    pythonProject2 Backup

---
+++ practice 1.py
@@ -0,0 +1,19 @@
+x="nirupama"
+x+"malik"
+print(x)
+
+l=["a","b","c","d"]
+l.append([5,6,7])
+print(l)
+
+l=["a","b","c","d"]
+l.extend([5,6,7])
+print(l)
+
+l=["a","b","c","d"]
+l.clear()
+print(l)
+
+l=["a","b","c","d"]
+del l[2]
+print(l)
